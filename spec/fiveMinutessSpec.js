e8f6c3cc191c1c37be736204019d0a5373396c19
Fétu Thimoté <thimote.fetu@student.vinci.be>
2024-11-07 19:33:23 +0100

    Add: fiveMinutes return [true, false, false, false, false, false, false, false, false, false, false] when given 5

--- spec/fiveMinutessSpec.js
+++ spec/fiveMinutessSpec.js
@@ -8,4 +8,10 @@ describe("FiveMinutes", function () {
 
         expect(result).toEqual([false, false, false, false, false, false, false, false, false, false, false]);
     });
+
+    it("should return [true, false, false, false, false, false, false, false, false, false, false] for 5", function () {
+        const result = main.fiveMinutes(main.intToMinutes(5));
+
+        expect(result).toEqual([true, false, false, false, false, false, false, false, false, false, false]);
+    });
 });
